b0a7db3771970a3da118e145fd26cab695795a95
nareiqat <nabeelareiqat@gmail.com>
2024-06-13 15:47:55 -0400

    Creating Account and tests complete!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,24 @@
 import {
   BrowserRouter as Router,
-  Routes,
   Route,
+  Routes,
   Navigate,
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
+import SignIn from "./pages/SignIn";
+// import AddHotel from "./pages/AddHotel";
+// import { useAppContext } from "./contexts/AppContext";
+// import MyHotels from "./pages/MyHotels";
+// import EditHotel from "./pages/EditHotel";
+// import Search from "./pages/Search";
+// import Detail from "./pages/Detail";
+// import Booking from "./pages/Booking";
+// import MyBookings from "./pages/MyBookings";
+// import Home from "./pages/Home";
 
 const App = () => {
+  // const { isLoggedIn } = useAppContext();
   return (
     <Router>
       <Routes>
@@ -15,7 +26,8 @@ const App = () => {
           path="/"
           element={
             <Layout>
-              <p>Homepage</p>
+              {/* <Home /> */}
+              <p>Home</p>
             </Layout>
           }
         />
@@ -23,7 +35,17 @@ const App = () => {
           path="/search"
           element={
             <Layout>
-              <p>SearchPage</p>
+              {/* <Search /> */}
+              <p>Search</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/detail/:hotelId"
+          element={
+            <Layout>
+              {/* <Detail /> */}
+              <p>Detail</p>
             </Layout>
           }
         />
@@ -35,10 +57,64 @@ const App = () => {
             </Layout>
           }
         />
+        <Route
+          path="/sign-in"
+          element={
+            <Layout>
+              <SignIn />
+            </Layout>
+          }
+        />
+
+        {/* {isLoggedIn && (
+          <>
+            <Route
+              path="/hotel/:hotelId/booking"
+              element={
+                <Layout>
+                  <Booking />
+                </Layout>
+              }
+            />
+
+            <Route
+              path="/add-hotel"
+              element={
+                <Layout>
+                  <AddHotel />
+                </Layout>
+              }
+            />
+            <Route
+              path="/edit-hotel/:hotelId"
+              element={
+                <Layout>
+                  <EditHotel />
+                </Layout>
+              }
+            />
+            <Route
+              path="/my-hotels"
+              element={
+                <Layout>
+                  <MyHotels />
+                </Layout>
+              }
+            />
+            <Route
+              path="/my-bookings"
+              element={
+                <Layout>
+                  <MyBookings />
+                </Layout>
+              }
+            />
+          </>
+        )} */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
 };
 
-export default App;
+export default App
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,16 +1,22 @@
 import { useForm } from "react-hook-form";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from "../api-client";
+import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
 
 export type RegisterFormData = {
-  email: string;
-  password: string;
   firstName: string;
   lastName: string;
+  email: string;
+  password: string;
   confirmPassword: string;
 };
 
 const Register = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { showToast } = useAppContext();
+
   const {
     register,
     watch,
@@ -19,16 +25,18 @@ const Register = () => {
   } = useForm<RegisterFormData>();
 
   const mutation = useMutation(apiClient.register, {
-    onSuccess: () => {
-      console.log("register successful!");
+    onSuccess: async () => {
+      showToast({ message: "Registration Success!", type: "SUCCESS" });
+      await queryClient.invalidateQueries("validateToken");
+      navigate("/");
     },
     onError: (error: Error) => {
-      console.log(error.message);
+      showToast({ message: error.message, type: "ERROR" });
     },
   });
 
   const onSubmit = handleSubmit((data) => {
-    mutation.mutate(data)
+    mutation.mutate(data);
   });
 
   return (
@@ -45,7 +53,7 @@ const Register = () => {
             <span className="text-red-500">{errors.firstName.message}</span>
           )}
         </label>
-        <label htmlFor="" className="text-gray-700 text-sm font-bold flex-1">
+        <label className="text-gray-700 text-sm font-bold flex-1">
           Last Name
           <input
             className="border rounded w-full py-1 px-2 font-normal"
@@ -56,62 +64,53 @@ const Register = () => {
           )}
         </label>
       </div>
-      <div className="flex flex-col md:flex-row gap-5">
-        <label className="text-gray-700 text-sm font-bold flex-1">
-          Email
-          <input
-            type="email"
-            className="border rounded w-full py-1 px-2 font-normal"
-            {...register("email", { required: "This field is required" })}
-          ></input>
-          {errors.email && (
-            <span className="text-red-500">{errors.email.message}</span>
-          )}
-        </label>
-      </div>
-      <div className="flex flex-col md:flex-row gap-5">
-        <label className="text-gray-700 text-sm font-bold flex-1">
-          Password
-          <input
-            type="password"
-            className="border rounded w-full py-1 px-2 font-normal"
-            {...register("password", {
-              required: "This field is required",
-              minLength: {
-                value: 6,
-                message: "Passowrd must be atleast 6 characters",
-              },
-            })}
-          ></input>
-          {errors.password && (
-            <span className="text-red-500">{errors.password.message}</span>
-          )}
-        </label>
-      </div>
-      <div className="flex flex-col md:flex-row gap-5">
-        <label className="text-gray-700 text-sm font-bold flex-1">
-          Confirm Password
-          <input
-            type="password"
-            className="border rounded w-full py-1 px-2 font-normal"
-            {...register("confirmPassword", {
-              required: "This field is required",
-              validate: (val) => {
-                if (!val) {
-                  return "This field is required";
-                } else if (watch("password") !== val) {
-                  return "Your passwords do no match";
-                }
-              },
-            })}
-          ></input>
-          {errors.confirmPassword && (
-            <span className="text-red-500">
-              {errors.confirmPassword.message}
-            </span>
-          )}
-        </label>
-      </div>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Email
+        <input
+          type="email"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("email", { required: "This field is required" })}
+        ></input>
+        {errors.email && (
+          <span className="text-red-500">{errors.email.message}</span>
+        )}
+      </label>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Password
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("password", {
+            required: "This field is required",
+            minLength: {
+              value: 6,
+              message: "Password must be at least 6 characters",
+            },
+          })}
+        ></input>
+        {errors.password && (
+          <span className="text-red-500">{errors.password.message}</span>
+        )}
+      </label>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Confirm Password
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("confirmPassword", {
+            validate: (val) => {
+              if (!val) {
+                return "This field is required";
+              } else if (watch("password") !== val) {
+                return "Your passwords do no match";
+              }
+            },
+          })}
+        ></input>
+        {errors.confirmPassword && (
+          <span className="text-red-500">{errors.confirmPassword.message}</span>
+        )}
+      </label>
       <span>
         <button
           type="submit"
